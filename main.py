fedfb9412fefae3d72ed754c519a0cb4294af8bf
PC-20241021WOVA\Administrator <240892907@qq.com>
2025-04-25 03:43:30 +0800

    add nltk.download('punkt_tab')

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from apps.router import (fileanalysis)
 
+
+import nltk
+nltk.download('punkt_tab')
+
 app = FastAPI()
 
 app.include_router(fileanalysis.router, prefix="/api/v1/file", tags=["fileanalysis"])
